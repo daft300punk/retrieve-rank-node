221ac4ad9fc59bbdbe9adca65a37202108143543
Abhilash Singh <daft300punk@gmail.com>
2017-06-23 18:36:14 +0530

    update routes

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,36 @@
  * Contains all application endpoints
  */
 import WeatherDataController from './controllers/WeatherDataController';
+import MockSalesController from './controllers/MockDataController';
 
 export default {
-  '/weatherData/:latitude/:longitude/:timeInterval/:timePeriod': {
+  '/weatherForecast/:latitude/:longitude/:timeInterval/:timePeriod': {
     get: {
-      method: WeatherDataController.weatherCompanyData,
+      method: WeatherDataController.weatherForecast,
+      public: true,
+    },
+  },
+  '/weatherAlerts/:latitude/:longitude': {
+    get: {
+      method: WeatherDataController.alerts,
+      public: true,
+    },
+  },
+  '/weatherHistory/:latitude/:longitude/:startDate/:endDate': {
+    get: {
+      method: WeatherDataController.weatherHistory,
+      public: true,
+    },
+  },
+  '/location/:latitude/:longitude': {
+    get: {
+      method: WeatherDataController.getLocation,
+      public: true,
+    },
+  },
+  '/mockData': {
+    get: {
+      method: MockSalesController.getMockSales,
       public: true,
     },
   },
