2b1a465b3eab5751eddb3884e966fb4bb7701f31
Abhilash Singh <daft300punk@gmail.com>
2017-06-23 18:35:17 +0530

    More functionality in weatherdata service

--- src/services/WeatherDataService.js
+++ src/services/WeatherDataService.js
@@ -1,15 +1,38 @@
 import decorate from 'decorate-it';
 import Joi from 'joi';
 import fetch from 'isomorphic-fetch';
-import buildWeatherUrl from '../util/buildWeatherUrl';
 
-function getWeatherData(geocode, timeInterval, timePeriod) {
-  const url = buildWeatherUrl(geocode, timeInterval, timePeriod);
+const hostname = `https://${process.env.UNAME}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/api/weather`;
+
+function buildGeocodeString(geocode) {
+  return `geocode/${geocode.latitude}/${geocode.longitude}`;
+}
+
+function getWeatherForecast(geocode, timeInterval, timePeriod) {
+  const geocodeString = buildGeocodeString(geocode);
+  const url = `${hostname}/v1/${geocodeString}/forecast/${timeInterval}/${timePeriod}.json`;
   return fetch(url);
 }
 
-getWeatherData.params = ['geocode', 'timeInterval', 'timePeriod'];
-getWeatherData.schema = {
+function getAlerts(geocode) {
+  const geocodeString = buildGeocodeString(geocode);
+  const url = `${hostname}/v1/${geocodeString}/alerts.json`;
+  return fetch(url);
+}
+
+function getWeatherHistory(geocode, { startDate, endDate }) {
+  const geocodeString = buildGeocodeString(geocode);
+  const url = `${hostname}/v1/${geocodeString}/almanac/daily.json?units=e&start=${startDate}&end=${endDate}`;
+  return fetch(url);
+}
+
+function getLocation(geocode) {
+  const url = `${hostname}/v3/location/point?geocode=${geocode.latitude}%2c${geocode.longitude}&language=en-US`;
+  return fetch(url);
+}
+
+getWeatherForecast.params = ['geocode', 'timeInterval', 'timePeriod'];
+getWeatherForecast.schema = {
   geocode: Joi.object().keys({
     latitude: Joi.string(),
     longitude: Joi.string(),
@@ -18,8 +41,39 @@ getWeatherData.schema = {
   timePeriod: Joi.string(),
 };
 
+getAlerts.params = ['geocode'];
+getAlerts.schema = {
+  geocode: Joi.object().keys({
+    latitude: Joi.string(),
+    longitude: Joi.string(),
+  }),
+};
+
+getWeatherHistory.params = ['geocode', 'timePeriod'];
+getWeatherHistory.schema = {
+  geocode: Joi.object().keys({
+    latitude: Joi.string(),
+    longitude: Joi.string(),
+  }),
+  timePeriod: Joi.object().keys({
+    startDate: Joi.string(),
+    endDate: Joi.string(),
+  }),
+};
+
+getLocation.params = ['geocode'];
+getLocation.schema = {
+  geocode: Joi.object().keys({
+    latitude: Joi.string(),
+    longitude: Joi.string(),
+  }),
+};
+
 const WeatherCompanyDataService = {
-  getWeatherData,
+  getWeatherForecast,
+  getAlerts,
+  getWeatherHistory,
+  getLocation,
 };
 
 decorate(WeatherCompanyDataService, 'WeatherCompanyDataService');
