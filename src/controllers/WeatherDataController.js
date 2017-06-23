75c7fad1d608c0c492588621a9d4403e22b2fdc4
Abhilash Singh <daft300punk@gmail.com>
2017-06-23 18:35:44 +0530

    Update weather data controller

--- src/controllers/WeatherDataController.js
+++ src/controllers/WeatherDataController.js
@@ -1,16 +1,58 @@
-import WeatherCompanyService from '../services/WeatherCompanyService';
+import WeatherDataService from '../services/WeatherDataService';
 
-async function weatherCompanyData(req, res) {
-  const geocode = {
-    latitude: req.params.latitude,
-    longitude: req.params.longitude,
-  };
+const getGeocodeFromReq = req => ({
+  latitude: req.params.latitude,
+  longitude: req.params.longitude,
+});
+
+async function weatherForecast(req, res) {
+  const geocode = getGeocodeFromReq(req);
   const timeInterval = req.params.timeInterval;
   const timePeriod = req.params.timePeriod;
 
   try {
-    let result = await WeatherCompanyService
-      .getWeatherData(geocode, timeInterval, timePeriod);
+    let result = await WeatherDataService
+      .getWeatherForecast(geocode, timeInterval, timePeriod);
+    result = await result.json();
+    res.json(result);
+  } catch (e) {
+    res.status(520).send(e);
+  }
+}
+
+async function alerts(req, res) {
+  const geocode = getGeocodeFromReq(req);
+  try {
+    let result = await WeatherDataService
+      .getAlerts(geocode);
+    result = await result.json();
+    res.json(result);
+  } catch (e) {
+    res.status(520).send(e);
+  }
+}
+
+async function weatherHistory(req, res) {
+  const geocode = getGeocodeFromReq(req);
+  const timePeriod = {
+    startDate: req.params.startDate,
+    endDate: req.params.endDate,
+  };
+  try {
+    let result = await WeatherDataService
+      .getWeatherHistory(geocode, timePeriod);
+    result = await result.json();
+    res.json(result);
+  } catch (e) {
+    res.status(520).send(e);
+  }
+}
+
+async function getLocation(req, res) {
+  const geocode = getGeocodeFromReq(req);
+  try {
+    let result = await WeatherDataService
+      .getLocation(geocode);
     result = await result.json();
     res.json(result);
   } catch (e) {
@@ -19,5 +61,8 @@ async function weatherCompanyData(req, res) {
 }
 
 export default {
-  weatherCompanyData,
+  weatherForecast,
+  alerts,
+  weatherHistory,
+  getLocation,
 };
