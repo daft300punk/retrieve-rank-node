c903df07a43212c9de4e098c2448f76b6364aa25
Abhilash Singh <daft300punk@gmail.com>
2017-06-24 19:30:49 +0530

    Update new solr routes

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@
  */
 import WeatherDataController from './controllers/WeatherDataController';
 import MockSalesController from './controllers/MockDataController';
+import SolrController from './controllers/SolrController';
 
 export default {
   '/weatherForecast/:latitude/:longitude/:timeInterval/:timePeriod': {
@@ -32,6 +33,54 @@ export default {
   '/mockData': {
     get: {
       method: MockSalesController.getMockSales,
+      public: true,
+    },
+  },
+  '/solr/availableClusters': {
+    get: {
+      method: SolrController.getAvailableClusters,
+      public: true,
+    },
+  },
+  '/solr/uploadData': {
+    get: {
+      method: SolrController.uploadData,
+      public: true,
+    },
+  },
+  '/solr/uploadSolrConfig/:clusterId': {
+    get: {
+      method: SolrController.uploadSolrConfig,
+      public: true,
+    },
+  },
+  '/solr/deleteSolrConfig/:clusterId': {
+    get: {
+      method: SolrController.deleteSolrConfig,
+      public: true,
+    },
+  },
+  '/solr/createSolrCollection/:clusterId': {
+    get: {
+      method: SolrController.createSolrCollection,
+      public: true,
+    },
+  },
+  '/solr/deleteSolrCollection/:clusterId': {
+    get: {
+      method: SolrController.deleteSolrCollection,
+      public: true,
+    },
+  },
+  '/solr/uploadSalesData/:clusterId': {
+    get: {
+      method: SolrController.uploadSalesData,
+      public: true,
+    },
+  },
+  '/solr/querySolr/:clusterId': {
+    get: {
+      method: SolrController.querySolr,
       public: true,
     },
   },
